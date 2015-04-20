ebe9ef89637ea26f4a2c4e85ca6bf9a4714adea7
Martin Koistinen <mkoistinen@gmail.com>
2015-04-20 22:11:07 +0300

    Add tests for default_reverse()

--- aldryn_newsblog/tests/test_utils.py
+++ aldryn_newsblog/tests/test_utils.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.core.urlresolvers import NoReverseMatch, reverse
+
 from unittest import TestCase
 
-from aldryn_newsblog.utils import add_prefix_to_path
+from ..utils import add_prefix_to_path, default_reverse
 
 
 class TestAddPrefixToPath(TestCase):
@@ -14,3 +20,38 @@ class TestAddPrefixToPath(TestCase):
         self.assertEqual(
             add_prefix_to_path('directory/template.html', 'prefix'),
             'directory/prefix/template.html')
+
+    def test_default_reverse(self):
+        non_pattern = 'invalid_url_pattern'
+
+        # First, test that the normal reverse works as expected (this proves
+        # that our setup conditions make sense)
+        with self.assertRaises(NoReverseMatch):
+            reverse(non_pattern)
+
+        # Test that default_reverse without a default set, acts the same
+        # as above.
+        with self.assertRaises(NoReverseMatch):
+            default_reverse(non_pattern)
+
+        # Now prove that default_reverse WITH a default returns that default.
+        # Trying with multiple types of values
+        class MyClass():
+            pass
+
+        defaults = [
+            'some_value', '', 'Undefined',
+            1, 0,
+            True, False,
+            None, MyClass, MyClass(),
+        ]
+
+        try:
+            for default in defaults:
+                self.assertEquals(
+                    default_reverse(non_pattern, default=default),
+                    default
+                )
+        except:
+            self.fail('default_reverse raised exception even though we set a '
+                      'default value.')
